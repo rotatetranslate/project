6ee4d9e8edece86c5250e55255ee805f927f897a
Adam Berro <adamberro@gmail.com>
2016-05-18 17:15:04 -0700

    add jump sound, 2 players

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var ctx = canvas.getContext('2d');
 // D A T A
 
 // var camera;
-// save(); translate(-camera.x, -camera.y); drawItAll(); restore();
+// save(); translate(-camera.x, -camera.y); draw(); restore();
 
 var width = 800;
 var height = 400;
@@ -29,6 +29,26 @@ var player = {
     ctx.fillRect(player.x, player.y, player.width, player.height);
   }
 };
+
+var player2 = {
+  x: width - 100,   // x axis position
+  y: height - 20, // y axis position
+  width: 10,
+  height: 20,
+  speed: 6,
+  velX: 0,
+  velY: 0,
+  jumping: false,
+  grounded: false,
+  //jumping: 0,
+  draw: function() {
+    ctx.fillStyle = 'rgb(255, 162, 167)';
+    ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
+  }
+};
+
+var p1jump = document.getElementById('p1-jump');
+var p2jump = document.getElementById('p2-jump');
 
 var keys = [];
 
@@ -118,6 +138,7 @@ function move() {
   if (keys[38] || keys[32]) {
     if (!player.jumping && player.grounded) {
       player.jumping = true;
+      p1jump.play();
       player.grounded = false;
       player.velY = -player.speed*2;
 
@@ -151,6 +172,49 @@ function move() {
 
   player.velX *= friction; //.875
   player.velY += gravity;  // .6
+}
+
+function move2() {
+  ////////// try adding numJumps. when not jump/grounded
+
+  // up arrow/space
+  if (keys[87]) {
+    if (!player2.jumping && player2.grounded) {
+      player2.jumping = true;
+      p2jump.play();
+      player2.grounded = false;
+      player2.velY = -player2.speed*2;
+
+    }
+  };
+
+  // if (keys[38] || keys[32]) {
+  //   player.jumping = 0;
+  //   player.jumping++;
+  //   if (player.jumping < 3) {
+  //     console.log('jump', player.jumping);
+  //     player.velY = -player.speed*2;
+  //     console.log('velY', player.velY);
+  //     console.log('speed', player.speed);
+  //   }
+  // };
+
+  // right arrow
+  if (keys[68]) {
+    if (player2.velX < player2.speed) {
+      player2.velX++;
+    }
+  };
+
+  // left arrow
+  if (keys[65]) {
+    if (player2.velX > -player2.speed) {
+      player2.velX--;
+    }
+  };
+
+  player2.velX *= friction; //.875
+  player2.velY += gravity;  // .6
 }
 
 
@@ -212,12 +276,19 @@ function update() {
   move();
   player.grounded = false;
 
+  player2.draw();
+  move2();
+  player2.grounded = false;
+
   ctx.fillStyle = "black";
   ctx.beginPath();
   for (var i = 0; i < boxes.length; i++) {
     ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
 
     var dir = collisionCheck(player, boxes[i]);
+    var dir2 = collisionCheck(player2, boxes[i]);
+    var dirP = collisionCheck(player, player2);
+    var dirP2 = collisionCheck(player2, player);
 
     if (dir === "l" || dir === "r") {
       player.velX = 0;
@@ -228,14 +299,31 @@ function update() {
     } else if (dir === "t") {
       player.velY *= -1;
     }
+
+    if (dir2 === "l" || dir2 === "r") {
+      player2.velX = 0;
+      player2.jumping = false;
+    } else if (dir2 === "b") {
+      player2.grounded = true;
+      player2.jumping = false;
+    } else if (dir2 === "t") {
+      player2.velY *= -1;
+    }
   }
 
   if (player.grounded){
     player.velY = 0;
   }
 
+  if (player2.grounded){
+    player2.velY = 0;
+  }
+
   player.x += player.velX;
   player.y += player.velY;
+
+  player2.x += player2.velX;
+  player2.y += player2.velY;
 
   ctx.fill();
 
